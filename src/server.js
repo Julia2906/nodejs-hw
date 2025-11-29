3b40b17368702a8c0c3a5fdf6cdf83468e9da87f
Yuliia <mykytenko.julia@gmail.com>
2025-11-29 16:48:46 +0200

    done

--- src/server.js
+++ src/server.js
@@ -8,6 +8,8 @@ import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js'
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
@@ -18,6 +20,10 @@ app.use(logger);
 app.use(express.json());
 
 app.use(cors());
+
+app.use(cookieParser());
+
+app.use(authRoutes);
 
 app.use(notesRoutes);
 
